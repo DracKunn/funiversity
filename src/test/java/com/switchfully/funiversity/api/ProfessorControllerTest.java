e7d368921692ccddd440cafd83b2922c1bacf49f
DracKunn <brecht.vanrie@gmail.com>
2022-06-01 16:49:28 +0200

    writing create professor test and building classes as we go

--- src/test/java/com/switchfully/funiversity/api/ProfessorControllerTest.java
+++ src/test/java/com/switchfully/funiversity/api/ProfessorControllerTest.java
@@ -1,9 +1,11 @@
 package com.switchfully.funiversity.api;
 
 import com.switchfully.funiversity.api.dtos.CreateProfessorDto;
+import com.switchfully.funiversity.api.dtos.ProfessorDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import io.restassured.RestAssured;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,5 +18,12 @@ class ProfessorControllerTest {
         CreateProfessorDto createProfessorDto = new CreateProfessorDto()
                 .setFirstName("Bob")
                 .setLastName("Villain");
+
+        ProfessorDto professorDto =
+                RestAssured.given()
+                        .body(createProfessorDto)
+                        .accept(JSON)
     }
+
+
 }
